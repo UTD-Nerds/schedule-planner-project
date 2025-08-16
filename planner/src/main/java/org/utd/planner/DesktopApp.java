73dca41d07d07a6925b2baa2f8969208f73dfab9
Khang Doan <kdoan.personal@gmail.com>
2025-08-16 01:49:25 -0500

    initialize Spring Boot + JavaFX

---
+++ planner/src/main/java/org/utd/planner/DesktopApp.java
@@ -0,0 +1,22 @@
+package org.utd.planner;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class DesktopApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/templates/fxml/hello-view.fxml"));
+        stage.setScene(new Scene(root));
+        stage.setTitle("Planner App");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ planner/src/main/java/org/utd/planner/WebpageApp.java
@@ -0,0 +1,13 @@
+package org.utd.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpageApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebpageApp.class, args);
+    }
+    
+}
