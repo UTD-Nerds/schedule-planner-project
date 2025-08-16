6083ac0585de594bb406b44b45a33e4d29f45e9c
Khang Doan <kdoan.personal@gmail.com>
2025-08-16 14:29:14 -0500

    update pom.xml

--- planner/src/main/java/org/utd/planner/DesktopApp.java
+++ planner/src/main/java/org/utd/planner/DesktopApp.java
@@ -7,7 +7,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class DesktopApp extends Application {
 
     @Override
@@ -21,4 +20,5 @@ public class DesktopApp extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
 }
