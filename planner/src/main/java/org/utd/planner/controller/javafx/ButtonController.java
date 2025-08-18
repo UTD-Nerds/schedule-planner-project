075982c291cab3765d513214170f20425bf630d7
Khang Doan <kdoan.personal@gmail.com>
2025-08-18 02:32:16 -0500

    modify controller folder

---
+++ planner/src/main/java/org/utd/planner/controller/javafx/ButtonController.java
@@ -0,0 +1,13 @@
+package org.utd.planner.controller.javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ButtonController {
+    @FXML Button button;
+
+    @FXML
+    public void print() {
+        System.out.println("Hello World!");
+    }
+}
