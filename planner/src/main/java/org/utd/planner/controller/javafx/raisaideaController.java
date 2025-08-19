d490e0d007092724c8f3bbdcb0f077638d616c0a
Raisa Reza <raisa16reza@gmail.com>
2025-08-19 00:38:22 -0500

    schedule button rough draft

---
+++ planner/src/main/java/org/utd/planner/controller/javafx/raisaideaController.java
@@ -0,0 +1,14 @@
+package org.utd.planner.controller.javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class raisaideaController {
+
+    @FXML Button viewSchedule;
+
+    public void method() {
+        System.out.println("You can view your schedule here.");
+    }
+    
+}
