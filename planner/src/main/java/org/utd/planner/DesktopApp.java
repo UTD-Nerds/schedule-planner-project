d97330be9b5f2dd6d95c2cdf87a29fc749d6991a
Natalia Sekulic <natalia.sekulic1@gmail.com>
2025-08-21 13:04:38 -0500

    natalia testing

--- planner/src/main/java/org/utd/planner/DesktopApp.java
+++ planner/src/main/java/org/utd/planner/DesktopApp.java
@@ -11,7 +11,7 @@ public class DesktopApp extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/templates/fxml/raisaidea.fxml")); 
+        Parent root = FXMLLoader.load(getClass().getResource("/templates/fxml/nataliaidea.fxml")); 
         stage.setScene(new Scene(root));                        //change fxml name to run
         stage.setTitle("Planner App");
         stage.show();
--- planner/src/main/java/org/utd/planner/controller/javafx/raisaideaController.java
+++ planner/src/main/java/org/utd/planner/controller/javafx/raisaideaController.java
@@ -2,13 +2,21 @@ package org.utd.planner.controller.javafx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
 public class raisaideaController {
 
     @FXML Button viewSchedule;
+    @FXML Button newSchedule;
+    @FXML Text helloWorld;
 
     public void method() {
-        System.out.println("You can view your schedule here.");
+        System.out.println("You can't view your schedule here.");
+    }
+
+    public void bark() {
+        System.out.println("BARK BARK BARK");
+        helloWorld.setText("Your Name");
     }
     
 }
