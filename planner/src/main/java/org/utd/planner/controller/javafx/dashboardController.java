fe4d4c726ba5362c777c03dd13b8dcb5564183d3
Khang Doan <kdoan.personal@gmail.com>
2025-08-22 23:29:18 -0500

    add dashboard.fxml & controller

---
+++ planner/src/main/java/org/utd/planner/controller/javafx/dashboardController.java
@@ -0,0 +1,15 @@
+package org.utd.planner.controller.javafx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+
+public class dashboardController {
+    
+    @FXML
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+    
+}
